0a5e10a1f8e5fa4c20fd88ad0a6f53e1f5dc1ee9
Diogo Barroso <ei11105@fe.up.pt>
2017-11-08 17:50:32 +0000

    Removed spaceship from repo - too big.

---
+++ Assets/PlayerBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour {
+    float BulletsPerSecond = 2;
+    float RateOfFire;
+	// Use this for initialization
+	void Start () {
+        RateOfFire = 1.0f / BulletsPerSecond;
+        InvokeRepeating("Fire", 1.0f, RateOfFire);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Fire()
+    {
+        //TODO: Fire a missile (prefab in the folders)
+    }
+}
